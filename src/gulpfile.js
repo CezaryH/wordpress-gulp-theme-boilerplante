7416ac7a11eb238bcabb8b93614b77b0d960a21c
Cez <cezary.haller@gmail.com>
2014-06-15 22:10:42 +0200

    gulp build

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,7 +2,7 @@
 
 // Include project requirements.
 var gulp = require( 'gulp' ),
-	plugins = require('gulp-load-plugins')({ camelize: true });
+	p = require('gulp-load-plugins')();
 
 // Sets assets folders.
 var dirs = {
@@ -14,35 +14,64 @@ var dirs = {
 		img : '../img/',
 		fonts : '../fonts/',
 	},
-	js: '/js/scripts/',
-	jsLib: '/js/libs',
-	sass: '/sass',
-	images: '/img',
-	fonts: '/fonts'
+	js: './js/scripts/',
+	jsLib: './js/libs',
+	sass: './sass',
+	images: './img',
+	fonts: './fonts',
+	views : './views/',
+	controllers : './controllers'
 };
-
-gulp.task( 'scripts', function () {
-	// Hint all JavaScript.
-	gulp.src( dirs.js + '/*.js' )
-		.pipe( plugins.jshint() )
-		.pipe( plugins.jshint.reporter( 'default' ) )
-		.pipe( plugins.uglify() )
-		.pipe( gulp.dest(dirs.js));
-});
 
 gulp.task( 'sass', function () {
 	// Compile all SCSS files.
-	gulp.src( dirs.sass + '/*.scss' )
-		.pipe( plugins.sass({
+	
+	return gulp.src( dirs.sass + '/*.scss' )
+		.pipe(p.sass({
 			outputStyle: 'compressed'
-		}) )
-		.pipe( gulp.dest( dirs.css ) );
+		}))
+		.pipe(p.autoprefixer())
+		.pipe( gulp.dest( dirs.dest.css ) )
+		.pipe(p.size())
+		.pipe(p.notify("Compilation complete."));
+});
+
+gulp.task("bower-files", function(){
+    return p.bowerFiles()
+    	.pipe(gulp.dest(dirs.dest.jsLib));
+});
+
+gulp.task( 'scripts', function () {
+	// Hint all JavaScript.
+	return gulp.src( dirs.js + '/*.js' )
+		.pipe( p.jshint())
+		.pipe( p.jshint.reporter( p.jshintStylish() ) )
+		.pipe( p.uglify())
+		.pipe( gulp.dest(dirs.dest.js));
+});
+
+gulp.task('html', ['styles', 'scripts'], function () {
+    var jsFilter = $.filter('**/*.js');
+    var cssFilter = $.filter('**/*.css');
+
+    return gulp.src([dirs.controllers, dire.views])
+        .pipe($.useref.assets())
+        .pipe(jsFilter)
+        .pipe($.uglify())
+        .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+        .pipe($.csso())
+        .pipe(cssFilter.restore())
+        .pipe($.useref.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size());
 });
 
 gulp.task( 'optimize', function () {
 	// Optimize all images.
 	gulp.src( dirs.images + '/*.{png,jpg,gif}' )
-		.pipe( plugins.imagemin({
+		.pipe( p.imagemin({
 			optimizationLevel: 7,
 			progressive: true
 		}) )
